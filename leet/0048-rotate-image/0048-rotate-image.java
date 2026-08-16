5cd0810fc1f7c30cd9e83d27d4b36bfd5dacfc29
PiloFrancy_Augustin <pilofrancy21@gmail.com>
2026-08-16 16:26:01 +0530

    Time: 0 ms (100%), Space: 43.9 MB (34.65%) - LeetHub-Neo

---
+++ leet/0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,24 @@
+class Solution {
+    static void reverse(int[] arr){
+        int left=0,right=arr.length-1 ;
+        while(left<right){
+            int temp=arr[left];
+            arr[left]=arr[right];
+            arr[right]=temp;
+            left++;right--;
+        }
+    }
+    public void rotate(int[][] matrix) {
+        for(int i=0;i<matrix.length-1;i++){
+            for(int j=i+1;j<matrix[0].length;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+                
+            }
+        }
+        for(int ind=0;ind<matrix.length;ind++){
+            reverse(matrix[ind]);
+        }
+    }
+}
