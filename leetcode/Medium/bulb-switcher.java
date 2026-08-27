c73fa493ed87edf6c868d7b9d021b88d0c64f277
PiloFrancy_Augustin <pilofrancy21@gmail.com>
2026-08-27 16:24:00 +0530

    Solved: Bulb Switcher (leetcode)

---
+++ leetcode/Medium/bulb-switcher.java
@@ -0,0 +1,13 @@
+// Problem: Bulb Switcher
+// Platform: leetcode
+// Rating/Difficulty: Medium
+// Language: java
+// Verdict: Accepted
+// URL: https://leetcode.com/problems/bulb-switcher/
+// Solved on: 2026-08-27T10:53:56.971Z
+
+class Solution {
+    public int bulbSwitch(int n) {
+        return(int)Math.sqrt(n);
+    }
+}
