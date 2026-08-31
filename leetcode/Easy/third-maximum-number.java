1e3e707f96bf058817a74f13fc82217ce7923f41
PiloFrancy_Augustin <pilofrancy21@gmail.com>
2026-08-31 19:45:15 +0530

    Solved: Third Maximum Number (leetcode)

---
+++ leetcode/Easy/third-maximum-number.java
@@ -0,0 +1,39 @@
+// Problem: Third Maximum Number
+// Platform: leetcode
+// Rating/Difficulty: Easy
+// Language: java
+// Verdict: Accepted
+// URL: https://leetcode.com/problems/third-maximum-number/
+// Solved on: 2026-08-31T14:15:14.487Z
+
+class Solution {
+    public int thirdMax(int[] nums) {
+        Integer first = null;
+        Integer second = null;
+        Integer third = null;
+
+        for (int num : nums) {
+
+            if ((first != null && num == first) ||
+                (second != null && num == second) ||
+                (third != null && num == third)) {
+                continue;
+            }
+
+            if (first == null || num > first) {
+                third = second;
+                second = first;
+                first = num;
+            }
+            else if (second == null || num > second) {
+                third = second;
+                second = num;
+            }
+            else if (third == null || num > third) {
+                third = num;
+            }
+        }
+
+        return third == null ? first : third;
+    }
+}
