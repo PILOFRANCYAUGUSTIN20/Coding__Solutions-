4636208ca692a202b037e0ffd08a90d9a856dfa2
PiloFrancy_Augustin <pilofrancy21@gmail.com>
2026-09-05 10:40:42 +0530

    Solved: Java 1D Array (hackerrank)

---
+++ hackerrank/Practice/java-1d-array-introduction.java
@@ -0,0 +1,12 @@
+// Problem: Java 1D Array
+// Platform: hackerrank
+// Language: java
+// Verdict: Accepted
+// URL: https://www.hackerrank.com/challenges/java-1d-array-introduction/problem?isFullScreen=true
+// Solved on: 2026-09-05T05:10:45.566Z
+
+
+        int[] a = new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=scan.nextInt();
+        }
