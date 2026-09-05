3d781ff41c037b366f03843d98bec8db5b2c26c2
PiloFrancy_Augustin <pilofrancy21@gmail.com>
2026-09-05 10:34:02 +0530

    Solved: Java Static Initializer Block (hackerrank)

---
+++ hackerrank/Practice/java-static-initializer-block.java
@@ -0,0 +1,22 @@
+// Problem: Java Static Initializer Block
+// Platform: hackerrank
+// Language: java
+// Verdict: Accepted
+// URL: https://www.hackerrank.com/challenges/java-static-initializer-block/problem?isFullScreen=true
+// Solved on: 2026-09-05T05:04:05.837Z
+
+
+    static int B;
+    static int H;
+    static boolean flag = true;
+    static {
+        Scanner ip = new Scanner (System.in);
+        B = ip.nextInt();
+        H = ip.nextInt();
+        if(B<0 || H<0){
+            System.out.print("java.lang.Exception: Breadth and height must be positive");
+            flag = false;
+        }
+    }
+    
+
