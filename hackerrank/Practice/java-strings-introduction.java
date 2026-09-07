ef7275a302dac5910615e8ee518f4bec12ef4c86
PiloFrancy_Augustin <pilofrancy21@gmail.com>
2026-09-07 14:28:57 +0530

    Solved: Java Strings Introduction (hackerrank)

---
+++ hackerrank/Practice/java-strings-introduction.java
@@ -0,0 +1,34 @@
+// Problem: Java Strings Introduction
+// Platform: hackerrank
+// Language: java
+// Verdict: Accepted
+// URL: https://www.hackerrank.com/challenges/java-strings-introduction/problem?isFullScreen=true
+// Solved on: 2026-09-07T08:58:57.159Z
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String B=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        System.out.println(A.length() + B.length());
+        if(A.compareTo(B)>0){
+            System.out.println("Yes");
+        }
+        else{
+            System.out.println("No");
+        }
+        String a = A.substring(0,1).toUpperCase() + A.substring(1);
+        String b = B.substring(0,1).toUpperCase() + B.substring(1);
+        System.out.println(a +" "+b);
+        
+    }
+}
+
+
+
