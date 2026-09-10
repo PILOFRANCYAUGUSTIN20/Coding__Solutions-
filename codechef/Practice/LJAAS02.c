fa6b89708efa6a50af9f0e1ca9ae15d9b1aeccb1
PiloFrancy_Augustin <pilofrancy21@gmail.com>
2026-09-10 14:05:07 +0530

    Solved: LJAAS02 (codechef)

---
+++ codechef/Practice/LJAAS02.c
@@ -0,0 +1,8 @@
+// Problem: LJAAS02
+// Platform: codechef
+// Language: Coding, Chef!
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/java/LPJAAS01/problems/LJAAS02
+// Solved on: 2026-09-10T08:35:08.278Z
+
+// source not captured automatically - copy it from the editor and use Manual Push
