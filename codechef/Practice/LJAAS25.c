8a3636352439d245a5b491ba08847ea555e0de37
PiloFrancy_Augustin <pilofrancy21@gmail.com>
2026-09-10 14:36:00 +0530

    Solved: LJAAS25 (codechef)

---
+++ codechef/Practice/LJAAS25.c
@@ -0,0 +1,12 @@
+// Problem: LJAAS25
+// Platform: codechef
+// Language: public class Main {
+    public static void main(String[] args) {
+        char ch = 'B';
+    }
+}
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/java/LPJAAS03/problems/LJAAS25
+// Solved on: 2026-09-10T09:06:02.036Z
+
+// source not captured automatically - copy it from the editor and use Manual Push
