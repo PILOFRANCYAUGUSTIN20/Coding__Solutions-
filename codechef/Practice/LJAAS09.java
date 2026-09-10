17d066da480d7c76f4daa8bab6c48ab860cc2b6b
PiloFrancy_Augustin <pilofrancy21@gmail.com>
2026-09-10 14:10:18 +0530

    Solved: LJAAS09 (codechef)

---
+++ codechef/Practice/LJAAS09.java
@@ -0,0 +1,12 @@
+// Problem: LJAAS09
+// Platform: codechef
+// Language: Java​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/java/LPJAAS01/problems/LJAAS09
+// Solved on: 2026-09-10T08:40:19.704Z
+
+class Codechef {
+    public static void main(String[] args) {
+        System.out.println(9*12);
+    }
+}
