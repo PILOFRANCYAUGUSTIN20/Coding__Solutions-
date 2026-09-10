56426e3b8fa50b8db673a1bcbbe40f59ea225245
PiloFrancy_Augustin <pilofrancy21@gmail.com>
2026-09-10 14:27:30 +0530

    Solved: LJAAS19 (codechef)

---
+++ codechef/Practice/LJAAS19.c
@@ -0,0 +1,13 @@
+// Problem: LJAAS19
+// Platform: codechef
+// Language: I love Codechef
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/java/LPJAAS02/problems/LJAAS19
+// Solved on: 2026-09-10T08:57:30.977Z
+
+public class Main {
+    public static void main(String[] args) {
+        // Your code here
+        System.out.println("I" + "\n" + "love" + "\n" + "CodeChef");
+    }
+}
