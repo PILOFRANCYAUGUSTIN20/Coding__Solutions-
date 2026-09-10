c0f09340ddc0da891f00fbd55878c6ea656c85c7
PiloFrancy_Augustin <pilofrancy21@gmail.com>
2026-09-10 14:16:12 +0530

    Solved: LJAAS14 (codechef)

---
+++ codechef/Practice/LJAAS14.c
@@ -0,0 +1,12 @@
+// Problem: LJAAS14
+// Platform: codechef
+// Language: public class Main {
+    public static void main(String[] args) {
+        System.out.println(7 + "\n" + 5);
+    }
+}
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/java/LPJAAS02/problems/LJAAS14
+// Solved on: 2026-09-10T08:46:12.932Z
+
+// source not captured automatically - copy it from the editor and use Manual Push
