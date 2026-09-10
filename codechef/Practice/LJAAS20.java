ba59a817eb26d23fe1991a8d5c97508cd672bf70
PiloFrancy_Augustin <pilofrancy21@gmail.com>
2026-09-10 14:30:29 +0530

    Solved: LJAAS20 (codechef)

---
+++ codechef/Practice/LJAAS20.java
@@ -0,0 +1,17 @@
+// Problem: LJAAS20
+// Platform: codechef
+// Language: Java​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/java/LPJAAS02/problems/LJAAS20
+// Solved on: 2026-09-10T09:00:30.982Z
+
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+		// your code goes here
+		for(int i=1;i<=5;i++){
+		    System.out.println(i + " - " + i*i);
+		}
+	}
+}
