6266e4c1d8f066264bb8f09eb4e18d2962934d87
PiloFrancy_Augustin <pilofrancy21@gmail.com>
2026-09-10 20:57:34 +0530

    Solved: Add Two Integers (leetcode)

---
+++ leetcode/Easy/add-two-integers.c
@@ -0,0 +1,11 @@
+// Problem: Add Two Integers
+// Platform: leetcode
+// Rating/Difficulty: Easy
+// Language: c
+// Verdict: Accepted
+// URL: https://leetcode.com/problems/add-two-integers/
+// Solved on: 2026-09-10T15:27:31.485Z
+
+int sum(int num1, int num2) {
+    return num1+num2;
+}
