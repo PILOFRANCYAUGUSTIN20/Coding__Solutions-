429c28ecc0902a6d2a92c5903a12ea2473da35b5
PiloFrancy_Augustin <pilofrancy21@gmail.com>
2026-09-10 14:17:38 +0530

    Solved: LJAAS17 (codechef)

---
+++ codechef/Practice/LJAAS17.c
@@ -0,0 +1,12 @@
+// Problem: LJAAS17
+// Platform: codechef
+// Language: public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello_" + "\n" + " World!");
+    }
+}
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/java/LPJAAS02/problems/LJAAS17
+// Solved on: 2026-09-10T08:47:39.342Z
+
+// source not captured automatically - copy it from the editor and use Manual Push
