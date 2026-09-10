ffe4e10189731bd349157b641d5437b834ff8995
PiloFrancy_Augustin <pilofrancy21@gmail.com>
2026-09-10 14:38:50 +0530

    Solved: LJAAS29 (codechef)

---
+++ codechef/Practice/LJAAS29.java
@@ -0,0 +1,14 @@
+// Problem: LJAAS29
+// Platform: codechef
+// Language: Java​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/java/LPJAAS03/problems/LJAAS29
+// Solved on: 2026-09-10T09:08:51.293Z
+
+public class Main {
+    public static void main(String[] args) {
+        // Write your code here 
+        int x=20 ,y=6;
+        System.out.println(x/y);
+    }
+}
