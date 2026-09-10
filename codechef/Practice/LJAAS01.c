b5dedeab66e80cf4fcc43b2f4c4a0df9ec242f58
PiloFrancy_Augustin <pilofrancy21@gmail.com>
2026-09-10 14:03:58 +0530

    Solved: LJAAS01 (codechef)

---
+++ codechef/Practice/LJAAS01.c
@@ -0,0 +1,12 @@
+// Problem: LJAAS01
+// Platform: codechef
+// Language: public class Main {
+    public static void main(String[] args) {
+        System.out.println(21 + 40);
+    }
+}
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/java/LPJAAS01/problems/LJAAS01
+// Solved on: 2026-09-10T08:33:59.893Z
+
+// source not captured automatically - copy it from the editor and use Manual Push
