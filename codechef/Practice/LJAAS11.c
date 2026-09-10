5dd96e84922d9e6c0a86104b9713c7bde41706eb
PiloFrancy_Augustin <pilofrancy21@gmail.com>
2026-09-10 14:12:37 +0530

    Solved: LJAAS11 (codechef)

---
+++ codechef/Practice/LJAAS11.c
@@ -0,0 +1,12 @@
+// Problem: LJAAS11
+// Platform: codechef
+// Language: public class Main {
+    public static void main(String[] args) {
+        System.out.println("Code" + "Chef");
+    }
+}
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/java/LPJAAS02/problems/LJAAS11
+// Solved on: 2026-09-10T08:42:38.202Z
+
+// source not captured automatically - copy it from the editor and use Manual Push
