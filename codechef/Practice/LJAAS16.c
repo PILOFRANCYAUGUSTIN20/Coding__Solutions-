e6215b48656107df41d8a631f9da7fad5b3465f6
PiloFrancy_Augustin <pilofrancy21@gmail.com>
2026-09-10 14:17:16 +0530

    Solved: LJAAS16 (codechef)

---
+++ codechef/Practice/LJAAS16.c
@@ -0,0 +1,12 @@
+// Problem: LJAAS16
+// Platform: codechef
+// Language: public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello " + "World" + 5 + " endl" + 2);
+    }
+}
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/java/LPJAAS02/problems/LJAAS16
+// Solved on: 2026-09-10T08:47:17.770Z
+
+// source not captured automatically - copy it from the editor and use Manual Push
