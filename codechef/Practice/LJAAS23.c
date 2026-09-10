1336fa11e2823de6a830d500109d1096dbaeab6a
PiloFrancy_Augustin <pilofrancy21@gmail.com>
2026-09-10 14:33:29 +0530

    Solved: LJAAS23 (codechef)

---
+++ codechef/Practice/LJAAS23.c
@@ -0,0 +1,12 @@
+// Problem: LJAAS23
+// Platform: codechef
+// Language: public class Main {
+    public static void main(String[] args) {
+        System.out.println(7.0 / 2);
+    }
+}
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/java/LPJAAS03/problems/LJAAS23
+// Solved on: 2026-09-10T09:03:30.898Z
+
+// source not captured automatically - copy it from the editor and use Manual Push
