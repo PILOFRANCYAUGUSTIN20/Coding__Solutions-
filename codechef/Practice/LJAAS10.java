0278905313fc8f525ffe0f002f935d5204f151d7
PiloFrancy_Augustin <pilofrancy21@gmail.com>
2026-09-10 14:12:00 +0530

    Solved: LJAAS10 (codechef)

---
+++ codechef/Practice/LJAAS10.java
@@ -0,0 +1,16 @@
+// Problem: LJAAS10
+// Platform: codechef
+// Language: Java​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/java/LPJAAS01/problems/LJAAS10
+// Solved on: 2026-09-10T08:42:01.710Z
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+        // Write your code here
+        System.out.println("Learn Coding on CodeChef");
+      
+	}
+}
