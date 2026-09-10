0dfa12b28b73c8bbb9aa657aa3eadafce3b9740c
PiloFrancy_Augustin <pilofrancy21@gmail.com>
2026-09-10 14:49:15 +0530

    Solved: LJAAS30 (codechef)

---
+++ codechef/Practice/LJAAS30.java
@@ -0,0 +1,16 @@
+// Problem: LJAAS30
+// Platform: codechef
+// Language: Java​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/java/LPJAAS03/problems/LJAAS30
+// Solved on: 2026-09-10T09:19:16.507Z
+
+public class Main {
+    public static void main(String[] args) {
+        // Your code goes here
+        double temperature = 25.5 ;
+        double kelvin = temperature+273;
+        System.out.println("Celsius - "+ temperature +"\n" +"Kelvin - "+ kelvin);
+        
+    }
+}
