6da594a7a7f015d12bad22794df75b75bbcaffaf
PiloFrancy_Augustin <pilofrancy21@gmail.com>
2026-09-10 14:15:52 +0530

    Solved: LJAAS13 (codechef)

---
+++ codechef/Practice/LJAAS13.c
@@ -0,0 +1,12 @@
+// Problem: LJAAS13
+// Platform: codechef
+// Language: public class Main {
+    public static void main(String[] args) {
+        System.out.println(8 + "\n" + 13 + " " + 21 + "\n");
+    }
+}
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/java/LPJAAS02/problems/LJAAS13
+// Solved on: 2026-09-10T08:45:53.771Z
+
+// source not captured automatically - copy it from the editor and use Manual Push
