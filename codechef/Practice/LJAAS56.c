835a382f05195dc4fd0ecc9216148ca0ca5f43ff
PiloFrancy_Augustin <pilofrancy21@gmail.com>
2026-09-12 11:26:05 +0530

    Solved: LJAAS56 (codechef)

---
+++ codechef/Practice/LJAAS56.c
@@ -0,0 +1,18 @@
+// Problem: LJAAS56
+// Platform: codechef
+// Language: public class Main {
+  public static void main(String[] args) {
+    int x = 5, y = 5;
+    if (x > y)
+      System.out.println("x is greater");
+    else if (y >= x)
+      System.out.println("y is greater");
+    else
+      System.out.println("Both are equal");
+  }
+}
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/java/LPJAAS06/problems/LJAAS56
+// Solved on: 2026-09-12T05:56:05.871Z
+
+// source not captured automatically - copy it from the editor and use Manual Push
