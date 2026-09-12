ef2078511a337f9ff60f3850da4d6096cb57444f
PiloFrancy_Augustin <pilofrancy21@gmail.com>
2026-09-12 11:12:24 +0530

    Solved: INSTNOODLE (codechef)

---
+++ codechef/Practice/INSTNOODLE.java
@@ -0,0 +1,23 @@
+// Problem: INSTNOODLE
+// Platform: codechef
+// Language: Java​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/java/PJA01/problems/INSTNOODLE
+// Solved on: 2026-09-12T05:42:25.861Z
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner ip = new Scanner (System.in);
+        int x = ip.nextInt();
+        int y = ip.nextInt();
+        int z = x*y;
+        System.out.println(z);
+	}
+}
