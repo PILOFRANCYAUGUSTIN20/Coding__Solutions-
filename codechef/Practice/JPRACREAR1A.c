f87324c12e5b07222357064d29f138358e4fcdf6
PiloFrancy_Augustin <pilofrancy21@gmail.com>
2026-09-12 11:09:40 +0530

    Solved: JPRACREAR1A (codechef)

---
+++ codechef/Practice/JPRACREAR1A.c
@@ -0,0 +1,12 @@
+// Problem: JPRACREAR1A
+// Platform: codechef
+// Language: Scanner read = new Scanner(System.in);
+String a = read.nextInt();
+String b = read.nextInt();
+String c = a + b;
+System.out.print(c);
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/java/PJA01/problems/JPRACREAR1A
+// Solved on: 2026-09-12T05:39:41.366Z
+
+// source not captured automatically - copy it from the editor and use Manual Push
