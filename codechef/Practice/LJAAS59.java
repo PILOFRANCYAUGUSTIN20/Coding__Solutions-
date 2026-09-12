ff3b91e21f5e535df468db57d432961d7a386f73
PiloFrancy_Augustin <pilofrancy21@gmail.com>
2026-09-12 11:31:07 +0530

    Solved: LJAAS59 (codechef)

---
+++ codechef/Practice/LJAAS59.java
@@ -0,0 +1,53 @@
+// Problem: LJAAS59
+// Platform: codechef
+// Language: Java​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/java/LPJAAS06/problems/LJAAS59
+// Solved on: 2026-09-12T06:01:08.256Z
+
+import java.util.Scanner;
+
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        Scanner scanner = new Scanner(System.in);
+        int num;
+        num = scanner.nextInt(); // input first number
+        // Check first number
+        if(num>0){
+            System.out.println("Positive");
+        }
+        else if (num<0){
+            System.out.println("Negative");
+        }
+        else{
+            System.out.println("Zero");
+        }
+        
+        num = scanner.nextInt(); // input second number
+        // Check second number
+        if(num>0){
+            System.out.println("Positive");
+        }
+        else if (num<0){
+            System.out.println("Negative");
+        }
+        else{
+            System.out.println("Zero");
+        }
+        
+        num = scanner.nextInt(); // input third number
+        // Check third number
+        if(num>0){
+            System.out.println("Positive");
+        }
+        else if (num<0){
+            System.out.println("Negative");
+        }
+        else{
+            System.out.println("Zero");
+        }
+        
+    }
+}
