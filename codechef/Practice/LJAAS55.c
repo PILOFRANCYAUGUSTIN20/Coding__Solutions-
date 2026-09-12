b6dfed6108f091a76abaa980616354300dc5cda6
PiloFrancy_Augustin <pilofrancy21@gmail.com>
2026-09-12 11:25:07 +0530

    Solved: LJAAS55 (codechef)

---
+++ codechef/Practice/LJAAS55.c
@@ -0,0 +1,16 @@
+// Problem: LJAAS55
+// Platform: codechef
+// Language: public class Main {
+  public static void main(String[] args) {
+    int a = 15;
+    if (a > 10)
+      System.out.print("Greater than 10");
+    if (a > 5)
+      System.out.print("Greater than 5");
+  }
+}
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/java/LPJAAS06/problems/LJAAS55
+// Solved on: 2026-09-12T05:55:08.214Z
+
+// source not captured automatically - copy it from the editor and use Manual Push
