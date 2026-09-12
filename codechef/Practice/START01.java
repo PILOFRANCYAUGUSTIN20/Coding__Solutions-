36020ccab997d8e77558bc066c543090f05c7a46
PiloFrancy_Augustin <pilofrancy21@gmail.com>
2026-09-12 11:07:56 +0530

    Solved: START01 (codechef)

---
+++ codechef/Practice/START01.java
@@ -0,0 +1,21 @@
+// Problem: START01
+// Platform: codechef
+// Language: Java​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/java/PJA01/problems/START01
+// Solved on: 2026-09-12T05:37:56.968Z
+
+import java.util.Scanner; 
+class Codechef
+{
+	public static void main (String[] args)
+	{
+	    // Do not print anything here before taking input
+	    Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		// Print the value of n
+		System.out.println(n);
+		
+	}
+}
