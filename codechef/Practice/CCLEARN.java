cc10d0260bfa72c58caf03b7e87aa8aac508ae7c
PiloFrancy_Augustin <pilofrancy21@gmail.com>
2026-09-12 11:20:51 +0530

    Solved: CCLEARN (codechef)

---
+++ codechef/Practice/CCLEARN.java
@@ -0,0 +1,22 @@
+// Problem: CCLEARN
+// Platform: codechef
+// Language: Java​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/java/PJA01/problems/CCLEARN
+// Solved on: 2026-09-12T05:50:52.214Z
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner ip = new Scanner (System.in);
+		int N = ip.nextInt();
+		System.out.println(N*2);
+
+	}
+}
