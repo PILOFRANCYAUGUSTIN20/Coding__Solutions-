608bbb363d788a5551db9744f26f42c91469e9df
PiloFrancy_Augustin <pilofrancy21@gmail.com>
2026-09-12 11:22:52 +0530

    Solved: LJAAS52 (codechef)

---
+++ codechef/Practice/LJAAS52.c
@@ -0,0 +1,17 @@
+// Problem: LJAAS52
+// Platform: codechef
+// Language: public class Main {
+    public static void main(String[] args) {
+       int marks = 75;
+       char grade;
+       if (marks >= 60)
+         grade = 'A';
+       else
+         grade = 'B';
+    }
+}
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/java/LPJAAS06/problems/LJAAS52
+// Solved on: 2026-09-12T05:52:52.652Z
+
+// source not captured automatically - copy it from the editor and use Manual Push
