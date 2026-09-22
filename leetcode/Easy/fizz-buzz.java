0821e7f52542c250b0792c4e6ca5252e3ded8e8a
PiloFrancy_Augustin <pilofrancy21@gmail.com>
2026-09-22 11:36:32 +0530

    Solved: Fizz Buzz (leetcode)

--- leetcode/Easy/fizz-buzz.java
+++ leetcode/Easy/fizz-buzz.java
@@ -4,13 +4,12 @@
 // Language: java
 // Verdict: Accepted
 // URL: https://leetcode.com/problems/fizz-buzz/
-// Solved on: 2026-09-22T06:05:04.409Z
+// Solved on: 2026-09-22T06:06:40.311Z
 
 class Solution {
     public List<String> fizzBuzz(int n) {
         List<String> list = new ArrayList<>();
         for(int i=1;i<=n;i++){
-            String str="";
             if(i%3 == 0 && i%5 == 0) 
                 list.add("FizzBuzz");
             else if(i%3 == 0)
@@ -18,8 +17,7 @@ class Solution {
             else if(i%5 == 0)
                 list.add("Buzz");
             else{
-                str+=i;
-                list.add(str);
+                list.add(String.valueOf(i));
             }
         }
         return list;
