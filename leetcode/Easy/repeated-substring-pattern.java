7d5a2cddf946a2744bd794531aef3d147fdccbbe
PiloFrancy_Augustin <pilofrancy21@gmail.com>
2026-09-22 20:00:02 +0530

    Solved: Repeated Substring Pattern (leetcode)

--- leetcode/Easy/repeated-substring-pattern.java
+++ leetcode/Easy/repeated-substring-pattern.java
@@ -4,13 +4,11 @@
 // Language: java
 // Verdict: Accepted
 // URL: https://leetcode.com/problems/repeated-substring-pattern/
-// Solved on: 2026-09-22T14:28:42.279Z
+// Solved on: 2026-09-22T14:30:00.347Z
 
 class Solution {
     public boolean repeatedSubstringPattern(String s) {
-
         String doubled = s + s;
-
         return doubled.substring(1, doubled.length() - 1).contains(s);
     }
 }
