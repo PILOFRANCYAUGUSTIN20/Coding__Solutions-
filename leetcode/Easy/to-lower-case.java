f34ffa000610c6f2858eab34850d0d196e426194
PiloFrancy_Augustin <pilofrancy21@gmail.com>
2026-09-23 10:23:37 +0530

    Solved: To Lower Case (leetcode)

---
+++ leetcode/Easy/to-lower-case.java
@@ -0,0 +1,13 @@
+// Problem: To Lower Case
+// Platform: leetcode
+// Rating/Difficulty: Easy
+// Language: java
+// Verdict: Accepted
+// URL: https://leetcode.com/problems/to-lower-case/
+// Solved on: 2026-09-23T04:53:35.263Z
+
+class Solution {
+    public String toLowerCase(String s) {
+        return s.toLowerCase();
+    }
+}
